7a435a08ba3df328301cb47e3afb0b972a592694
Letizia Palmas <lp645@cam.ac.uk>
2025-07-03 14:13:16 +0200

    trained model with SAC algorithm

--- new model/environment.py
+++ new model/environment.py
@@ -20,8 +20,9 @@ class PendulumVerticalEnv(gym.Env):
         "render_modes": ["human", "rgb_array"],
         "render_fps": 30}
 
-    def __init__(self, seism, T, dt=1e-3, episode_length=1500):
+    def __init__(self, seism, T, dt=1e-3, episode_length=350000, num_envs=1):
         super(PendulumVerticalEnv, self).__init__()
+        self.num_envs = num_envs
         self.seism = seism
         self.dt = dt
         self.T = T 
@@ -93,6 +94,8 @@ class PendulumVerticalEnv(gym.Env):
         self.interp_velocity = interp1d(self.tt_data, seism, kind='linear',
                                                 bounds_error=False, fill_value=0.0)(self.tt_sim)
         
+        self.v0 = self.seism[3000]  # initial velocity
+        self.x0 = self.zt[3000]  # initial displacement
         #self.force_levels = np.linspace(0, 10, 11)
         #force_values = np.linspace(-10, 10, 20)
         #self.action_space = spaces.Discrete(20)
@@ -111,10 +114,10 @@ class PendulumVerticalEnv(gym.Env):
         self.observation_space = spaces.Box(obs_low, obs_high, dtype=np.float32)
 
 
-    def reset(self, seed: Optional[int] = None):
+    def reset(self, seed: Optional[int] = 42):
         self.done = False
         self.current_step = 0
-        self.state = np.array([0.] * 6, dtype=np.float32)
+        self.state = np.array([self.v0, 0., 0., self.x0, 0., 0.], dtype=np.float32)
         for key in self.history:
             self.history[key] = []
         return np.array(self.state, dtype=np.float32), {}
@@ -131,7 +134,7 @@ class PendulumVerticalEnv(gym.Env):
         #force_values = np.linspace(-10., 10., 20)
         #self.control_force = force_values[action]
         self.control_force = float(action * self.max_force_mag) 
-
+        prev_displacement = self.state[5]
         seismic_input = self.interp_displacement[self.current_step]
         force = self.K[0] * seismic_input
         total_force = force + self.control_force
@@ -145,53 +148,66 @@ class PendulumVerticalEnv(gym.Env):
 
         output = self.state[5]  # vertical displacement
 
-        reward = self._compute_reward_smooth_displacement(output)
+        reward = self._compute_reward(prev_displacement, output)
         self.history["reward"].append(reward)
 
         self.current_step += 1
         terminated = bool(abs(self.state[5]) > 10)
-        truncated = bool(self.current_step >= self.episode_length )
+        truncated = bool(self.current_step > self.episode_length )
         self.done = terminated or truncated
 
         obs = np.array(self.state, dtype=np.float32)
 
         return obs, reward, terminated, truncated, {"displacement":output}
     
-    def _compute_reward(self, current_disp):
-        if self.current_step == 0:
+    def _compute_reward(self, prev_disp, current_disp):
+        if current_disp == 0:
             return 0.0  # No reward at the first step
-        alpha = 100.0
-        main_reward = math.exp(-alpha * current_disp**2)
-        progress_bonus = 0.0
-        if len(self.history["x6"]) >= 2:
-            prev_disp = self.history["x6"][-2]
-            improvement = abs(prev_disp) - abs(current_disp)
-        if improvement > 0:
-            # beta scales the importance of the progress bonus
-            beta = 1.0 
-            progress_bonus = beta * improvement
+        disp = abs(current_disp)
+        improvement = abs(prev_disp) - disp
+        beta = 1000.0 
+        progress_bonus = beta * improvement
+        #print(f'Progress bonus = {beta} * {improvement} = {progress_bonus}')
 
-        return main_reward + progress_bonus 
+        if current_disp <= 1e-5 and current_disp >= -1e-9:
+            #print(f'Current displacement: {current_disp}')
+            return 100.0 
+        # elif disp <= 1e-3 and disp > 1e-6:
+        #      return 1.0 
+        # elif disp <= 1e-3 and disp > 1e-4:
+        #     return 0.3 
+        # elif disp <= 1e-2 and disp > 1e-3:
+        #     return 0.1 
+        # elif disp <= 9e-1 and disp > 1e-2:
+        #     return -1.0
+        else:
+            return 0.0
+        # alpha = 10.0
+        # main_reward = np.exp(-alpha * current_disp**2)
+        # progress_bonus = 0.0
+        # if len(self.history["x6"]) >= 2:
+        #     prev_disp = self.history["x6"][-2]
+        #     improvement = abs(prev_disp) - abs(current_disp)
+        #     if improvement > 1e-6:
+        #     # beta scales the importance of the progress bonus
+        #         beta = 1.0 
+        #         progress_bonus = beta * improvement
+
+        # return main_reward 
   
         
-    def _compute_reward_smooth_displacement(self, displacement):
-        """Smooth exponential reward - better for RL training"""
-        abs_disp = abs(displacement)
+    def _compute_reward_smooth_displacement(self, prev_disp, current_displacement):
+        """Smooth exponential reward"""
+        abs_disp = abs(current_displacement)
         # Exponential decay with scaling
-        k = 1e6  # Adjust this based on your displacement scale
+        k = 10000  # Adjust this based on your displacement scale
         reward = np.exp(-k * abs_disp)
-        return reward
+        improvement = abs(prev_disp) - abs(current_displacement)
+        beta = 500.
+        final_reward = reward + (beta * improvement)
+        return final_reward 
 
 
-    # def _compute_reward(self, x6):
-    #     if abs(x6) < 1e-6:
-    #         return 1.0
-    #     elif abs(x6) < 1e-3:
-    #         return 0.5
-    #     elif abs(x6) < 1e-1:
-    #         return 0.1
-    #     else:
-    #         return 0.0
 
     def render(self, mode='human'):        
         print(f"""
--- new model/rl_model.py
+++ new model/rl_model.py
@@ -2,63 +2,146 @@ from environment import PendulumVerticalEnv
 import numpy as np
 import h5py
 import gymnasium as gym
-from stable_baselines3 import DQN, PPO
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3 import SAC
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 from stable_baselines3.common.monitor import Monitor
 
+np.random.seed(42)
+
+T = 370
+dt = 1e-3
+Nt_step = T * 1e3
+tmax = Nt_step * dt  
+end = int(T * 62.5)
+
+episode_length = 370000  # Total number of steps in the episode
 f = h5py.File("/Users/letizia/Desktop/INFN/new model/SaSR_test.hdf5", "r")
 seism = f['SR/V1:ENV_CEB_SEIS_V_dec'][:] #seismic data
+seism = seism[:end]*1e-6
 
-seism = seism[:12500]*1e-6
+tt_data = np.linspace(0, T, len(seism))
+tt_sim = np.arange(0, tmax, dt)
 
-env = PendulumVerticalEnv(seism, T = 200, dt = 1e-3, episode_length = 300000) 
-env = Monitor(env)
-#env = gym.make('CartPole-v1', render_mode = 'human')  
-#Validate custom environment
-
-#env.reset()
-
-#print(f"Initial observation shape: {env.observation_space.shape}")
-print(f"Observation space: {env.observation_space}")
-print(f"Action space: {env.action_space}")
-
-
-model = PPO(
-    "MlpPolicy", 
-    env, 
-    gamma = 0.99, 
-    batch_size = 32, 
-    learning_rate= 1e-4, 
-    verbose=1, 
-    tensorboard_log="./ppo_tensorboard/"
-    )
-
-model.learn(total_timesteps=800000, progress_bar=True)
+env = PendulumVerticalEnv(seism, T = T, dt = dt, episode_length = episode_length) 
 
 obs, info = env.reset()
-for i in range(300000):
+
+#model = SAC.load("sac_pendulum_vertical_control_5", env=env)
+model = SAC(
+    "MlpPolicy", 
+    env, 
+    gamma = 0.9, 
+    batch_size = 256, 
+    learning_rate= 1e-4, 
+    verbose=1, 
+    tensorboard_log="./sac_control/"
+    )
+
+model.learn(total_timesteps=370000, progress_bar=True)
+print("Training completed.")
+model.save("sac_pendulum_vertical_control_6")
+
+#obs, info = env.reset()
+
+controlled_displacements = []
+for i in range(100000):
     action, _states = model.predict(obs)
     obs, rewards, terminated, truncated, info = env.step(action)
+    controlled_displacements.append(info['displacement'])
     env.render()
-    # if i % 10 == 0:
-    #     print(f"\nStep: {i}, "
-                
-    #             f"\nAction: {action}, "
-    #             f"\nReward: {rewards:.3f}")
-    # print(f"Displacement: {info['displacement']:.3e}")
-    # print(f"Seismic input: {info['seismic_input']:.3e}")
-    # print(f"Action taken: {action}, Reward: {rewards}")
+
     
     if terminated or truncated:
-        obs, _ = env.reset()
+        obs, info = env.reset()
+        #break
+
+
+def calculate_transfer_function(input_signal, output_signal, dt):
+    """Helper function to compute the FFT-based transfer function.
+    Input data in time domain"""
+    input_signal = np.array(input_signal)
+    output_signal = np.array(output_signal)
+    window = np.hanning(len(input_signal))
+
+    freqs = np.fft.fftfreq(len(input_signal), d=1/62.5)
+    
+    fft_input = np.fft.fft(input_signal )
+    fft_output = np.fft.fft(output_signal )
+    #freqs = np.fft.fftfreq(n, d=dt)
+    
+    transfer_function = np.abs(fft_output/ fft_input)
+    print(len(input_signal), len(output_signal))
+    return freqs, transfer_function
 
 x6       = np.array(env.history["x6"])       # vertical displacements
 rewards  = np.array(env.history["reward"])   # step-by-step rewards
 actions  = np.array(env.history["force"])    # control forces you applied
 steps    = np.array(env.history["step"])  
 
+# Ensure all lists have the same length for FFT
+#min_len = min(len(seismic_inputs), len(uncontrolled_displacements), len(controlled_displacements))
+#seismic_inputs = seismic_inputs[:min_len]
+#uncontrolled_displacements = uncontrolled_displacements[:min_len]
+#controlled_displacements = controlled_displacements[:min_len]
+vf = np.fft.fft(seism)
+
+
+frequencies = np.fft.fftfreq(len(seism), d = 1/62.5)
+half = len(frequencies) // 2 #half of the frequencies array (positive frequencies only)
+
+xf = vf[1:] / (1j * 2 * np.pi * frequencies[1:]) # Fourier Transform of the seismic data
+X_f = np.zeros_like(vf, dtype=complex) #create an array of zeros with the same shape as V
+nonzero = frequencies != 0 #boolean mask: true if freq is not zero
+
+#for all non-zero frequencies, divide the FT by 2 pi f the take the IFT to get the displacement
+X_f[nonzero] = vf[nonzero] / (1j * 2 * np.pi * frequencies[nonzero])
+
+zt = np.fft.ifft(X_f).real
+xt = np.fft.ifft(xf) 
+#freqs, tf_uncontrolled = calculate_transfer_function(seismic_inputs, uncontrolled_displacements, dt)
+print(len(x6))
+
+#interpolate the controlled displacements to match the seismic data length
+disp_interp = interp1d(np.arange(len(x6)), x6,
+                       kind='linear', bounds_error=False, fill_value=0.0)
+x6_interp = disp_interp(tt_data)
+input_interp = interp1d(np.arange(len(zt)), zt,
+                        kind='linear', bounds_error=False, fill_value=0.0)(np.linspace(0, len(zt)-1, len(x6)))
+freqs, tf_controlled = calculate_transfer_function(zt, x6_interp, dt)
+
+half = len(freqs) // 2
+plt.figure(figsize=(14, 8))
+    
+# Plot 1: Transfer Functions
+plt.subplot(2, 1, 1)
+#positive_freqs_mask = (freqs > 0) & (freqs < 10) # Focus on relevant frequencies
+#plt.plot(freqs[:half], tf_uncontrolled[:half], label='Without Control')
+plt.plot(abs(frequencies), tf_controlled, label='PPO Control', linestyle='--')
+plt.plot(abs(frequencies), np.abs(X_f), label='Seismic Input (frequency domain)')
+plt.plot(abs(frequencies), np.abs(np.fft.fft(x6_interp)), label='output (freq domain)', linestyle=':')
+plt.title('System Transfer Function Comparison')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel(r'Transfer function ($\tilde{x}_6 / \tilde{x}_{in}$)')
+plt.yscale('log')
+plt.xscale('log')
+plt.legend()
+plt.grid(True)
+
+# Plot 2: Displacement over time
+plt.subplot(2, 1, 2)
+#time_axis = np.arange(min_len) * dt
+#plt.plot(time_axis, uncontrolled_displacements, label='Without Control', alpha=0.8)
+plt.plot(x6_interp, label='PPO Control', alpha=0.8)
+plt.title('Bottom Mass Displacement Over Time')
+plt.xlabel('Time (s)')
+plt.ylabel('Displacement (m)')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
 
 plt.figure()
 plt.subplot(3,1,1)
@@ -80,3 +163,4 @@ plt.show()
 # # evaluation_results = evaluate_policy(model, eval_env, n_eval_episodes=100, return_episode_rewards=True)
 # analysis = env.get_transfer_function_analysis()
 # env.plot_transfer_function_comparison(analysis)
+env.close()
